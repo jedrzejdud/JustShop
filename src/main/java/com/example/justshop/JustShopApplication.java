194200c0eb511d86eae62f529562cb148448a17e
jk22 <jedrzejdud00@gmail.com>
2023-03-23 14:41:54 +0100

    Initial commit

---
+++ src/main/java/com/example/justshop/JustShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.justshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustShopApplication.class, args);
+    }
+
+}
